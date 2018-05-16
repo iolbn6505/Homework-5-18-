e3981a3639d7b0d3bdcb746a317f603623eb8393
aiolbn500119 <aiolbn500119@gmail.com>
2018-05-16 18:55:01 +0800

    fixed2

--- main.c
+++ main.c
@@ -3,13 +3,13 @@
 
 
 int main() {
-	int sum=0,add;
-	for(add=0;add<100;add++)
+	int sum=0,add;//設定變數 
+	for(add=0;add<100;add++)//1加到99 
 	{
 		sum=sum+add;
 		
 	}
-	printf("sum=%d",sum);
+	printf("sum=%d",sum);//顯示總數 
 	 
 
 }
